e1c6f7dbd9c7ec991b4174741a51c5c79d4c14bb
saraweinand <saraweinand@gmail.com>
2014-10-20 13:36:54 -0700

    Base Commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+
+  $('#one').corner('rounded');
+  $('#two').corner('dog tr');
+  $('#three').corner('sharp bottom');
+
+});
